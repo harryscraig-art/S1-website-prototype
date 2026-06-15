ec704f613d85afc1106ba4d70b09b3596a51030f
v0agent <it+v0agent@vercel.com>
2026-06-15 00:40:28 +0000

    feat: replace Image component with img tag in LogoPlaceholder
    Replaced Next.js Image component with standard img tag for logo rendering

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,16 +1,11 @@
 'use client'
 
-import Image from 'next/image'
-
 export function LogoPlaceholder() {
   return (
     <a href="#" className="flex items-center hover:opacity-80 transition-opacity">
-      <Image 
+      <img 
         src="/logo.svg" 
         alt="Supportology Logo" 
-        width={500} 
-        height={120}
-        priority
         className="h-10 w-auto"
       />
     </a>
